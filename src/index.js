61f4b6d88908b6fa9425a033f536a0fa69f61281
Andrei Shauko <andrevvise@gmail.com>
2020-10-24 14:16:04 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const finalArray = [];
+    const brackets = Object.fromEntries(bracketsConfig);
+
+    for (let bracket of str.split('')) {
+        if (finalArray[0] === bracket) {
+            finalArray.shift();
+        } else if (brackets[bracket]) {
+            finalArray.unshift(brackets[bracket]);
+        } else {
+            return false;
+        }
+    }
+    return finalArray.length === 0;
 }
